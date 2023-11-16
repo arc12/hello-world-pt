7bf4cc83badcd221a42e7a62a6438c33309b8c8a
Adam Cooper <arc@hilltop-cottage.info>
2023-11-16 14:27:35 +0000

    config enablement of keep-warm timer + shorten HTTP GET timeout

--- HelloWorldTimer/__init__.py
+++ HelloWorldTimer/__init__.py
@@ -4,18 +4,23 @@ from os import environ
 
 import azure.functions as func
 
-from hello_world import PLAYTHING_NAME  # TODO add core and include a ping enable setting in core_config.json
+from hello_world import PLAYTHING_NAME, core
 
 def main(mytimer: func.TimerRequest) -> None:
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-    
-    url_base = environ.get("PLAYGROUND_PING_URL_BASE", None)  # e.g. = "https://dlpg-test1.azurewebsites.net"
+    if core.keep_warm:
+        if mytimer.past_due:
+            logging.info('The timer is past due!')
+        
+        url_base = environ.get("PLAYGROUND_PING_URL_BASE", None)  # e.g. = "https://dlpg-test1.azurewebsites.net"
 
-    if url_base is None:
-        logging.warn(f"Environ PLAYGROUND_PING_URL_BASE is not set; abort pinging {PLAYTHING_NAME}.")
-    else:
-        url = f"{url_base}/{PLAYTHING_NAME}/ping"
-        req = requests.get(url)
+        if url_base is None:
+            logging.warn(f"Environ PLAYGROUND_PING_URL_BASE is not set; abort pinging {PLAYTHING_NAME}.")
+        else:
+            url = f"{url_base}/{PLAYTHING_NAME}/ping"
+            try:
+                req = requests.get(url, timeout=10)
+                logging.info(f"Ping {url} from timerTrigger => HTTP {req.status_code}, Content: {req.text}")
+            except requests.exceptions.ConnectTimeout:
+                logging.warn("Request to {url} from timerTrigger timed out.")
+                exit(1)
 
-        logging.info(f"Ping {url} from timerTrigger => HTTP {req.status_code}, Content: {req.text}")
